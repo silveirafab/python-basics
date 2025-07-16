b167ef598a2938b620b71cf9427eda92444b9ec8
silveirafab <74255714+silveirafab@users.noreply.github.com>
2025-07-16 11:08:57 -0400

    adding classwork

---
+++ code-along-input-function.py
@@ -0,0 +1,8 @@
+first_name = input('Enter your first name: ')
+print(first_name)
+
+age = input('Enter your age: ')
+print(type(age))
+agenum = int(age)
+print(type(agenum))
+print(agenum + agenum)
---
+++ code-along-variables.py
@@ -0,0 +1,40 @@
+my_boolean = True
+my_int = 42
+my_float = 3.14159
+my_string = 'Assesss'
+my_string2 = "Educate"
+my_multi_line_string = """This
+is a
+multi-line string"""
+one_line = 'This is a one line string'
+
+print(my_boolean)
+print(one_line)
+print(my_multi_line_string)
+
+print(type(my_boolean))
+
+my_variable1 = None
+print(type(my_variable1))
+
+
+# Set up our variables
+num1 = 3.14
+num2 = 2
+num3 = 16.0
+
+# Create expressions and store their results
+add_result = num1 + num2
+subtract_result = num1 - num2
+multiply_result = num1 * num2
+divide_result = num3 / num2
+exponent_result = num1 ** num2
+remainder_result = num3 % num2
+greater_than = num1 > num2
+less_than = num1 < num2
+equal_to = num1 == num2
+multiple_ops = round((divide_result * 5) ** num1, 3)
+
+print(multiple_ops)
+print(remainder_result)
+print(greater_than)
